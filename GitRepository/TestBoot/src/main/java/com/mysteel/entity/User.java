ab2221dcf691a182adadbc8ed5dc3b4eef795898
Administrator <Administrator@admin-PC>
2017-04-05 11:29:13 +0800

    将User序列化，用于测试

--- GitRepository/TestBoot/src/main/java/com/mysteel/entity/User.java
+++ GitRepository/TestBoot/src/main/java/com/mysteel/entity/User.java
@@ -1,6 +1,9 @@
 package com.mysteel.entity;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private String name;
 	private String password;
